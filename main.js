e0bc4d2e3e4af9995ddb31e41c16228e1d6d1ebb
Kyaw1511 <kyawminthein151193@gmail.com>
2024-09-15 20:48:36 +0630

    toggle aboutPara and show and hide seeMore btn for mobile view

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // gyn projects;
+const seeMoreAbout = document.querySelector(".seeMorePara");
+const aboutPara = document.querySelector(".aboutPara2");
+const screenLogX = window.innerWidth;
+
+// function for seeMoreAboutPara;
+function seeMoreAboutPara() {
+    aboutPara.classList.toggle("active");
+    console.log("aboutPara");
+}
+if(screenLogX < 440) {
+    seeMoreAbout.style.display = "block";
+    console.log("hide");
+}
+else {
+    seeMoreAbout.style.display = "none";
+}
+
+
+// addEventListener for seeMore;
+seeMoreAbout.addEventListener("click", seeMoreAboutPara);
