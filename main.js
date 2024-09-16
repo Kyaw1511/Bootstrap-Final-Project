ada6b1c581a36efa2ac297fdfcbd800bbd4d81e7
Kyaw1511 <kyawminthein151193@gmail.com>
2024-09-16 21:36:13 +0630

    to build aboutImages V1.0

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // gyn projects;
 const seeMoreAbout = document.querySelector(".seeMorePara");
 const aboutPara = document.querySelector(".aboutPara2");
+const aboutImgs = document.querySelector(".aboutImg");
+
 const screenLogX = window.innerWidth;
+
 
 // function for seeMoreAboutPara;
 function seeMoreAboutPara() {
@@ -16,6 +19,37 @@ else {
     seeMoreAbout.style.display = "none";
 }
 
+// function for aboutImgs;
+function aboutImages() {
+    // aboutImgs.src = "images/gym25.jpeg";
+
+    console.log("hello");
+}
+
+
 
 // addEventListener for seeMore;
 seeMoreAbout.addEventListener("click", seeMoreAboutPara);
+// addEventListen for aboutImg;
+aboutImgs.addEventListener("click", aboutImages);
+
+const dataAboutimages = {
+    "items" : [
+        {
+            "id": 0,
+            "title": "image_1",
+            "src": "images/gym23.jpeg"
+        },
+        {
+            "id": 1,
+            "title": "image_1",
+            "src": "images/gym25.jpeg"
+        },
+        {
+            "id": 2,
+            "title": "image_2",
+            "src": "images/gym26.jpeg"
+        },
+
+    ]
+}
