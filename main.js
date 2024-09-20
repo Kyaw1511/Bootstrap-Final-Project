9af70186907a1588561a3eb25d15b74dd1868da9
Kyaw1511 <kyawminthein151193@gmail.com>
2024-09-20 21:03:50 +0630

    service scroll V1.0

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const seeMoreAbout = document.querySelector(".seeMorePara");
 const aboutPara = document.querySelector(".aboutPara2");
 const aboutImgs = document.querySelector(".aboutImg");
+const serviceColoumn = document.querySelector("#service-right-coloumn");
+const serviceRight = document.querySelector("#service-right");
 
 const screenLogX = window.innerWidth;
 let currentIndex = 0;
@@ -20,6 +22,15 @@ function aboutImages() {
     console.log("hello");
 }
 
+// function for serviceRightDetails;
+function serviceRightDetails() {
+    
+    console.log("serviceRightDetails Function");
+}
+
+serviceColoumn.style.border = "2px solid green";
+serviceColoumn.style.height = "1000px";
+
 // mobile site version
 if(screenLogX < 440) {
     seeMoreAbout.style.display = "block";
@@ -33,6 +44,8 @@ else {
 seeMoreAbout.addEventListener("click", seeMoreAboutPara);
 // addEventListen for aboutImg;
 aboutImgs.addEventListener("click", aboutImages);
+// addEventListen for serviceRight;
+serviceRight.addEventListener("click", serviceRightDetails);
 
 let dataAboutimages = [
     "images/gym26.jpeg",
@@ -44,29 +57,29 @@ let dataAboutimages = [
 
 let offerCategories = [
     {
-        "title": "testing1",
+        "title": "Retail Merchandise",
         "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore mollitia, deleniti consectetur, ut quaerat culpa optio fugit quas natus eligendi ullam soluta.",
-        "images": "images/gym26.jpeg"
+        "images": "images/gym25.jpeg"
     },
     {
-        "title": "testing2",
+        "title": "Online Personal Training",
         "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore mollitia, deleniti consectetur, ut quaerat culpa optio fugit quas natus eligendi ullam soluta.",
-        "images": "images/gym26.jpeg"
+        "images": "images/gym23.jpeg"
     },
     {
-        "title": "testing3",
+        "title": "Personal Training",
         "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore mollitia, deleniti consectetur, ut quaerat culpa optio fugit quas natus eligendi ullam soluta.",
-        "images": "images/gym26.jpeg"
+        "images": "images/gym27.jpeg"
     },
     {
-        "title": "testing4",
+        "title": "Solo Camp",
         "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore mollitia, deleniti consectetur, ut quaerat culpa optio fugit quas natus eligendi ullam soluta.",
-        "images": "images/gym26.jpeg"
+        "images": "images/gym2.jpeg"
     },
     {
-        "title": "testing5",
+        "title": "Q & A section",
         "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore mollitia, deleniti consectetur, ut quaerat culpa optio fugit quas natus eligendi ullam soluta.",
-        "images": "images/gym26.jpeg"
+        "images": "images/gym35.jpeg"
     }
 ]
 
